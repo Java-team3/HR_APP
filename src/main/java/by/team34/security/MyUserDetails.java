e595494a9bf24e9990879f8decc9aee8127e5ef6
Yan <yan.efimov@gmail.com>
2019-01-06 14:36:33 +0300

    final spring security

--- src/main/java/by/team34/security/MyUserDetails.java
+++ src/main/java/by/team34/security/MyUserDetails.java
@@ -10,36 +10,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import by.team34.entity.Role;
+import by.team34.dao.UserDao;
 import by.team34.entity.User;
-import by.team34.service.UserService;
 
 @Service
 public class MyUserDetails implements UserDetailsService {
 
   @Autowired
-  private UserService userRepository;
+  private UserDao userRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	User user = null;
-	  List<User> users = userRepository.findAll();
-	for (User i : users) {
-		if (i.getName().equals(username)) {
-			user = i;
-			break;
-		}
-	}
-    //final User user = userRepository.findByUsername(username);
-	for (Role role : user.getRoles()) {
-		System.out.println(role.getAuthority());
-	}
+    final User user = userRepository.getUserByName(username);
+    
     if (user == null) {
       throw new UsernameNotFoundException("User '" + username + "' not found");
     }
-    System.out.println();
+
     return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), user.getRoles());
-    //return (UserDetails) user;
   }
 
 }
